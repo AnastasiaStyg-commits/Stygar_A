008e421200c3dd7e73b703bafd1ef5805bc0bb92
AnastasiaStyg-commits <anastasiakoroly@gmail.com>
2026-07-22 20:58:44 +0300

    lesson_3.py

---
+++ homework/lesson_3.py
@@ -0,0 +1,16 @@
+# Переменные имя и город
+name = ("Stygar_A")
+city = "Moscow"
+
+# Вывод через print
+print (name)
+print(city)
+
+# Вывод на ячейки памяти по id
+print(id(name))
+print(id(city))
+
+#Вывод типа данный города
+print(type(city))
+
+
